eedec7169c14d9836212a603f6b9e62b47c26b4d
jennyliuca <jenny.liu.ca@gmail.com>
2023-05-03 20:15:00 -0700

    add pytest report

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # selenium_swag
 use selenium test https://www.saucedemo.com/
+
+## This test can run in macOS. Install requirement
+```shell
+make install
+```
+
+OR
+
+```shell
+pip install -r requirements.txt
+```
+
+
+## How to run the tests in macOS
+```shell
+make test
+```
+
+OR
+
+```shell
+python3 start_tests.py
+```
+
+OR run only selected test cases
+
+```shell
+python3 start_tests.py -t TEST00001
+```
+
+## How to view the report
+The test report should save in pytest_report.html
--- test_swag.py
+++ test_swag.py
@@ -1,11 +1,8 @@
 #import student
 
 import pytest
-# import os
 import logging
-# import time
 
-# from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -13,7 +10,6 @@ from selenium.webdriver.support import expected_conditions as EC
 from libs.login_page import *
 
 
-# logger = logging.getLogger("unit test")
 def verify_login(suite_setupteardown,
     eachtest_setupteardown,
     usernm,
@@ -31,7 +27,6 @@ def verify_login(suite_setupteardown,
     username.send_keys(usernm)
 
     password = loginpage.get_password()
-    # password.send_keys("secret_sauce")
     password.send_keys(passwd)
 
     login_button = loginpage.get_login_button()
@@ -44,7 +39,6 @@ def verify_login(suite_setupteardown,
             EC.presence_of_element_located((By.CLASS_NAME, "shopping_cart_link"))
         )
         
-        # time.sleep(10)
         logging.info(f'{driver.current_url}')
 
 
@@ -64,8 +58,68 @@ def verify_login(suite_setupteardown,
 
 
 @pytest.mark.TEST00001
+@pytest.mark.parametrize(
+    "dstring, eachtest_setupteardown, usernm, passwd, err_msg, expected_result",
+    [
+        ("TEST00001 : Verify login success : a valid username and password", ["debug"], "standard_user", "secret_sauce", "", True),
+    ],
+    indirect=["eachtest_setupteardown"],
+)
+def test_login_page_success(
+    suite_setupteardown,
+    eachtest_setupteardown,
+    dstring,
+    usernm,
+    passwd,
+    err_msg,
+    expected_result,
+):
+    test_login_page_success.__doc__ = dstring
+    actual_result = verify_login(suite_setupteardown, eachtest_setupteardown, usernm, passwd, err_msg)
+    assert actual_result == expected_result
+    
 @pytest.mark.TEST00002
+@pytest.mark.parametrize(
+    "dstring, eachtest_setupteardown, usernm, passwd, err_msg, expected_result",
+    [
+        ("TEST00002 : Verify login fail and error message : empty password", ["debug"], "standard_user", "", "Epic sadface: Password is required", True),
+    ],
+    indirect=["eachtest_setupteardown"],
+)
+def test_login_page_empty_password(
+    suite_setupteardown,
+    eachtest_setupteardown,
+    dstring,
+    usernm,
+    passwd,
+    err_msg,
+    expected_result,
+):
+    test_login_page_empty_password.__doc__ = dstring
+    actual_result = verify_login(suite_setupteardown, eachtest_setupteardown, usernm, passwd, err_msg)
+    assert actual_result == expected_result
+
 @pytest.mark.TEST00003
+@pytest.mark.parametrize(
+    "dstring, eachtest_setupteardown, usernm, passwd, err_msg, expected_result",
+    [
+        ("TEST00003 : Verify login fail and error message : empty username", ["debug"], "", "secret_sauce", "Epic sadface: Username is required", True),
+    ],
+    indirect=["eachtest_setupteardown"],
+)
+def test_login_page_empty_username(
+    suite_setupteardown,
+    eachtest_setupteardown,
+    dstring,
+    usernm,
+    passwd,
+    err_msg,
+    expected_result,
+):
+    test_login_page_empty_username.__doc__ = dstring
+    actual_result = verify_login(suite_setupteardown, eachtest_setupteardown, usernm, passwd, err_msg)
+    assert actual_result == expected_result
+
 @pytest.mark.TEST00004
 @pytest.mark.TEST00005
 @pytest.mark.TEST00006
@@ -75,11 +129,8 @@ def verify_login(suite_setupteardown,
 @pytest.mark.parametrize(
     "dstring, eachtest_setupteardown, usernm, passwd, err_msg, expected_result",
     [
-        ("TEST00001 : Verify login success : a valid username and password", ["debug"], "standard_user", "secret_sauce", "", True),
-        ("TEST00002 : Verify login fail and error message : an invalid username", ["debug"], "standard_userabc", "secret_sauce", "Epic sadface: Username and password do not match any user in this service", True),
-        ("TEST00003 : Verify login fail and error message : an invalid password", ["debug"], "standard_user", "0secret_sauce", "Epic sadface: Username and password do not match any user in this service", True),
-        ("TEST00004 : Verify login fail and error message : empty password", ["debug"], "standard_user", "", "Epic sadface: Password is required", True),
-        ("TEST00005 : Verify login fail and error message : empty username", ["debug"], "", "secret_sauce", "Epic sadface: Username is required", True),
+        ("TEST00004 : Verify login fail and error message : an invalid username", ["debug"], "standard_userabc", "secret_sauce", "Epic sadface: Username and password do not match any user in this service", True),
+        ("TEST00005 : Verify login fail and error message : an invalid password", ["debug"], "standard_user", "0secret_sauce", "Epic sadface: Username and password do not match any user in this service", True),
         ("TEST00006 : Verify login fail and error message : username ending space", ["debug"], "standard_user ", "secret_sauce", "Epic sadface: Username and password do not match any user in this service", True),
         ("TEST00007 : Verify login fail and error message : username leading space", ["debug"], " standard_user", "secret_sauce", "Epic sadface: Username and password do not match any user in this service", True),
         ("TEST00008 : Verify login fail and error message : password ending space", ["debug"], "standard_user", "secret_sauce  ", "Epic sadface: Username and password do not match any user in this service", True),
@@ -87,7 +138,7 @@ def verify_login(suite_setupteardown,
     ],
     indirect=["eachtest_setupteardown"],
 )
-def test_login_page(
+def test_login_page_unmatch(
     suite_setupteardown,
     eachtest_setupteardown,
     dstring,
@@ -96,8 +147,7 @@ def test_login_page(
     err_msg,
     expected_result,
 ):
-    test_login_page.__doc__ = dstring
-    # driver = suite_setupteardown
+    test_login_page_unmatch.__doc__ = dstring
     actual_result = verify_login(suite_setupteardown, eachtest_setupteardown, usernm, passwd, err_msg)
     assert actual_result == expected_result
-    
+                                
